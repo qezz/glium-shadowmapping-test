716ec88479713d6d1c84925441e76565e20bd8f6
Sergey Mishin <sergei.a.mishin@gmail.com>
2017-11-20 11:19:17 +0300

    Cleanup

--- src/aaa.rs
+++
@@ -1,9 +0,0 @@
-
-        let biasMatrix: cgmath::Matrix4<f32> = [[ 0.5_f32, 0.0, 0.0, 0.0, ],
-                                                [ 0.0, 0.5, 0.0, 0.0, ],
-                                                [ 0.0, 0.0, 0.5, 0.0, ],
-                                                [ 0.5, 0.5, 0.5, 1.0, ]].into();
-
-        let depthMVP = depthProjectionMatrix * depthViewMatrix * depthModelMatrix;
-
-        let depthBiasMVP = biasMatrix * depthMVP;
